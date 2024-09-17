65bb3024ff5ffaae0a42d0eb8477c3d84f4a5f66
matsindect <52255514+matsindect@users.noreply.github.com>
2024-09-17 11:24:34 +0400

    Update appError.js

--- backend/utils/appError.js
+++ backend/utils/appError.js
@@ -1,3 +1,17 @@
+/**
+ * Custom error class for handling application-specific errors.
+ * Extends the built-in Error class.
+ *
+ * @class AppError
+ * @extends {Error}
+ * 
+ * @param {string} message - The error message.
+ * @param {number} statusCode - The HTTP status code associated with the error.
+ * 
+ * @property {number} statusCode - The HTTP status code.
+ * @property {string} status - The error status ('fail' for 4xx codes, 'error' for others).
+ * @property {boolean} isOperational - Indicates if the error is operational (true).
+ */
 class AppError extends Error {
     constructor(message, statusCode) {
       super(message);
